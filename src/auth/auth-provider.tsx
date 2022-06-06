acf52e98732d0b5d599c0f99ace8ae5194e95898
mac <gos360@163.com>
2022-06-06 13:25:46 +0800

    feat:auth-provider 基本写法

--- src/auth/auth-provider.tsx
+++ src/auth/auth-provider.tsx
@@ -1,1 +1,30 @@
-export {}
+interface Form {
+    username: string;
+    password: string;
+}
+
+
+export const Login = (form: Form) => {
+    fetch(`http://localhost:3000/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(form)
+
+    }).then(r => r.json())
+}
+
+export const Register = (form: Form) => {
+    fetch(`http://localhost:3000/register`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(form)
+
+    }).then(r => r.json())
+}
+
+export const Logout = () => {
+}
