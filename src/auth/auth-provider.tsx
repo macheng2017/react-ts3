c03190fb22cf5b3aac02a44830895ddb824f81c3
mac <gos360@163.com>
2022-06-07 16:32:58 +0800

    add:env file

--- src/auth/auth-provider.tsx
+++ src/auth/auth-provider.tsx
@@ -3,28 +3,40 @@ interface Form {
     password: string;
 }
 
+interface User {
+    username: string;
+    password: string;
+    token: string;
+}
+const api_url = process.env.REACT_APP_API_URL || "http://localhost:3000";
+const handleUserResponse = (user: User) => {
+    window.localStorage.setItem("token",user.token);
+    return user;
+}
 
 export const Login = (form: Form) => {
-    fetch(`http://localhost:3000/login`, {
+    fetch(`${api_url}/login`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(form)
 
-    }).then(r => r.json())
+    }).then(async r => handleUserResponse(await r.json()));
 }
 
 export const Register = (form: Form) => {
-    fetch(`http://localhost:3000/register`, {
+    fetch(`${api_url}/register`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(form)
-
-    }).then(r => r.json())
+    }).then(
+        async r => handleUserResponse(await r.json())
+    )
 }
 
 export const Logout = () => {
+    window.localStorage.removeItem("token");
 }
